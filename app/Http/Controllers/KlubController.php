828f1f27240b1296d973ebfa48df965d8210b35a
Pertini245 <petariristic@gmail.com>
2021-12-29 11:10:50 +0100

    REST API zahtev, napravljene GET i DELETE rute za Klub, kreiran KlubController

---
+++ app/Http/Controllers/KlubController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\KlubResource;
+use App\Models\Klub;
+use Illuminate\Http\Request;
+
+class KlubController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $klubovi = Klub::all();
+        return KlubResource::collection($klubovi);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Klub  $klub
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Klub $klub)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Klub  $klub
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Klub $klub)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Klub  $klub
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Klub $klub)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Klub  $klub
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Klub $klub)
+    {
+        $klub->delete();
+        return response()->json('Klub je obrisan!');
+    }
+}
